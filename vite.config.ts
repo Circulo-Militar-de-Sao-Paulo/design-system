a99d38dc4435850c9cde9b1e6e7bff6e194da38a
Willian Pinheiro <willpinhaaa@gmail.com>
2024-06-24 11:47:43 -0300

    adiciona algumas configurações a mais de publish

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		dts({ include: ["src"], exclude: ["src/**/*.stories.tsx"] }),
+	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.ts"),
-			name: "design-system",
 			formats: ["es"],
 		},
 		rollupOptions: {
