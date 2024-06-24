bb2bfd73f295f8eee9b3438effd74e6ac8b3b585
Willian Pinheiro <willpinhaaa@gmail.com>
2024-06-24 12:33:34 -0300

    adiciona todos os exports no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,51 @@
-export { Button } from "./components/button";
+/*
+['accordion.tsx', 'alert-dialog.tsx', 'alert.tsx', 'aspect-ratio.tsx', 'avatar.tsx', 'badge.tsx', 'breadcrumb.tsx', 'button.tsx', 'calendar.tsx', 'card.tsx', 'carousel.tsx', 'checkbox.tsx', 'collapsible.tsx', 'command.tsx', 'context-menu.tsx', 'dialog.tsx', 'drawer.tsx', 'dropdown-menu.tsx', 'form.tsx', 'hover-card.tsx', 'input-otp.tsx', 'input.tsx', 'label.tsx', 'menubar.tsx', 'navigation-menu.tsx', 'pagination.tsx', 'popover.tsx', 'progress.tsx', 'radio-group.tsx', 'resizable.tsx', 'scroll-area.tsx', 'select.tsx', 'separator.tsx', 'sheet.tsx', 'skeleton.tsx', 'slider.tsx', 'sonner.tsx', 'switch.tsx', 'table.tsx', 'tabs.tsx', 'textarea.tsx', 'toast.tsx', 'toaster.tsx', 'toggle-group.tsx', 'toggle.tsx', 'tooltip.tsx', 'use-toast.ts']
+*/
+
+export * from "@/components/accordion";
+export * from "@/components/alert-dialog";
+export * from "@/components/alert";
+export * from "@/components/aspect-ratio";
+export * from "@/components/avatar";
+export * from "@/components/badge";
+export * from "@/components/breadcrumb";
+export * from "@/components/button";
+export * from "@/components/calendar";
+export * from "@/components/card";
+export * from "@/components/carousel";
+export * from "@/components/checkbox";
+export * from "@/components/collapsible";
+export * from "@/components/command";
+export * from "@/components/context-menu";
+export * from "@/components/dialog";
+export * from "@/components/drawer";
+export * from "@/components/dropdown-menu";
+export * from "@/components/form";
+export * from "@/components/hover-card";
+export * from "@/components/input-otp";
+export * from "@/components/input";
+export * from "@/components/label";
+export * from "@/components/menubar";
+export * from "@/components/navigation-menu";
+export * from "@/components/pagination";
+export * from "@/components/popover";
+export * from "@/components/progress";
+export * from "@/components/radio-group";
+export * from "@/components/resizable";
+export * from "@/components/scroll-area";
+export * from "@/components/select";
+export * from "@/components/separator";
+export * from "@/components/sheet";
+export * from "@/components/skeleton";
+export * from "@/components/slider";
+export * from "@/components/sonner";
+export * from "@/components/switch";
+export * from "@/components/table";
+export * from "@/components/tabs";
+export * from "@/components/textarea";
+export * from "@/components/toast";
+export * from "@/components/toaster";
+export * from "@/components/toggle-group";
+export * from "@/components/toggle";
+export * from "@/components/tooltip";
+export * from "@/components/use-toast";
