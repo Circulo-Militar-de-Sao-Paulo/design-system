42177b93392574068017f1bb592dc686688fff01
Willian Pinheiro <willpinhaaa@gmail.com>
2024-06-25 11:33:29 -0300

    arruma imagem do círculo do gh pages

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -4,6 +4,7 @@ export default create({
 	base: "dark",
 	brandTitle: "Círculo Militar de São Paulo",
 	brandUrl: "https://www.circulomilitar.org.br",
-	brandImage: "logo.svg",
+	brandImage:
+		"https://circulo-militar-de-sao-paulo.github.io/design-system/logo.svg",
 	brandTarget: "_blank",
 });
