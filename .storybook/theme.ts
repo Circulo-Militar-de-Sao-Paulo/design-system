809b716fa7a514bba8385674b31b1c5b02ec6548
Willian Pinheiro <willpinhaaa@gmail.com>
2024-06-25 11:30:17 -0300

    arruma imagem no github pages

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -4,6 +4,6 @@ export default create({
 	base: "dark",
 	brandTitle: "Círculo Militar de São Paulo",
 	brandUrl: "https://www.circulomilitar.org.br",
-	brandImage: "/logo.svg",
+	brandImage: "logo.svg",
 	brandTarget: "_blank",
 });
