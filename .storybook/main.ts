e4a42911953f208a8f9fdcde807293f36ead60d8
Willian Pinheiro <willpinhaaa@gmail.com>
2024-06-25 11:05:20 -0300

    atualiza base path do storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,5 +16,11 @@ const config: StorybookConfig = {
 		name: "@storybook/react-vite",
 		options: {},
 	},
+	viteFinal: (config, { configType }) => {
+		if (configType === "PRODUCTION") {
+			config.base = "/design-system/";
+		}
+		return config;
+	},
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/design-system/",
 	plugins: [
 		react(),
 		tsconfigPaths(),
