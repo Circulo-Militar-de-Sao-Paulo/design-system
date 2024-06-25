c8897e606dd747ef445362798c9878ff4dac596e
Willian Pinheiro <willpinhaaa@gmail.com>
2024-06-25 11:10:11 -0300

    muda versão

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,11 +16,6 @@ const config: StorybookConfig = {
 		name: "@storybook/react-vite",
 		options: {},
 	},
-	viteFinal: (config, { configType }) => {
-		if (configType === "PRODUCTION") {
-			config.base = "/design-system/";
-		}
-		return config;
-	},
+	staticDirs: ["../public"],
 };
 export default config;
