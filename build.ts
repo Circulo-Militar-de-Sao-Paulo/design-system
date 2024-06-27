52dc6e3d96bbcb9de85041d146f51b64b1b2b528
Willian Pinheiro <willpinhaaa@gmail.com>
2024-06-27 20:06:52 -0300

    arruma script build

--- build.ts
+++ build.ts
@@ -1,5 +1,9 @@
-await Bun.build({
-	entrypoints: ["src/index.tsx"],
-	outdir: "dist",
-	external: ["react", "react-dom", "mantine"],
-});
+async function build() {
+	await Bun.build({
+		entrypoints: ["src/index.tsx"],
+		outdir: "dist",
+		external: ["react", "react-dom", "mantine"],
+	});
+}
+
+build();
