50d13d2706b48cd2b0c7b9cc04cfaf8598d60a94
Willian Pinheiro <willpinhaaa@gmail.com>
2024-08-20 23:42:33 -0300

    cria logo com animação ou não

--- design/index.ts
+++ design/index.ts
@@ -1,4 +1,5 @@
 export { tailwindConfig } from "./lib/tailwind";
 
 export { Button } from "./components/button";
+export { Logo } from "./components/logo";
 export { Shell } from "./components/shell";
